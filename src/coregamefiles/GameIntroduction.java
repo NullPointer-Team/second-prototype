59ec6801404bb56a694d30f0d3613e3aba45a964
BruceBAWest <brucebawest@gmail.com>
2020-07-16 15:15:53 -0700

    add intro to game, ask for rules

--- src/coregamefiles/GameIntroduction.java
+++ src/coregamefiles/GameIntroduction.java
@@ -15,15 +15,11 @@ import java.util.Scanner;
 
 public class GameIntroduction {
     // This method will display the basic version of the game's rules and information.
-    public void basicGameInformation() {
+    public static void gameInformation() {
         welcomeMessage();
         String userInput = ruleDisplaySelection();
         if (userInput.equals("Y") || userInput.equals("YES")) {
-            System.out.println("\nWelcome to Stranger Game, a text-based Java console RPG game.  Please read the rules below:\n" +
-                    "Although the basic game has a lot of complexity to it, the rules to play it are simple.\n" +
-                    "You are trapped in a maze in a fantasy world, and in order to return to reality you must complete the game \n" +
-                    "before the portal closes. Navigate from room to room, collecting useful items and completing challenges \n" +
-                    "to unlock the portal. Hurry! The portal back to earth won't stay open long!\n" +
+            System.out.println(
                     "To navigate from room to room, type these commands:\n" +
                     "    \"go north\"\n" +
                     "    \"go south\"\n" +
@@ -41,7 +37,7 @@ public class GameIntroduction {
     }
 
     // This private method will prompt the user if they want to hear the rules for the game or not.
-    private String ruleDisplaySelection() {
+    private static String ruleDisplaySelection() {
         System.out.println("\nWould you like to hear the rules of the game?\n" +
                            "Type Y or Yes to hear the rules.\n" +
                            "Type anything else to continue to the game.\n");
@@ -51,9 +47,12 @@ public class GameIntroduction {
     }
 
     // This method contains the welcome information text.
-    private void welcomeMessage() {
-        System.out.println("Welcome to Stranger Game by team NullPointer.\n" +
-                           "This game was developed in Java by Neill Perry, Bruce West and Tapan Trivedi, and is available on GitHub.\n" +
-                           "https://github.com/NullPointer-Team\n");
+    private static void welcomeMessage() {
+        System.out.println(
+                "Welcome to Stranger Game, a text-based Java console RPG game.  Please read the rules below:\n" +
+                "Although the basic game has a lot of complexity to it, the rules to play it are simple.\n" +
+                "You are trapped in a maze in a fantasy world, and in order to return to reality you must complete the game \n" +
+                "before the portal closes. Navigate from room to room, collecting useful items and completing challenges \n" +
+                "to unlock the portal. Hurry! The portal back to earth won't stay open long!\n");
     }
 }
