88bd01d1675777f150f56b92e0678175576c4908
BruceBAWest <brucebawest@gmail.com>
2020-07-16 09:46:03 -0700

    obsess over game menu art alignment
    didn't change much. just added white space in the println

--- src/coregamefiles/GameMenuTextArt.java
+++ src/coregamefiles/GameMenuTextArt.java
@@ -14,19 +14,20 @@ package coregamefiles;
 public class GameMenuTextArt extends GameMenu {
 
     static void gameMenuDisplayText() {
-        System.out.println( "_______________________________________________________\n" +
-                            "|     ▄▄▌ ▐ ▄▄▄▄ ▄▄▌  ▄▄        ▌ ▄   ▄▄▄             |\n" +
-                            "|     ██  █▌▐▀▄ ▀██  ▐█ ▌      ██ ▐███▀▄ ▀            |\n" +
-                            "|     ██ ▐█▐▐▐▀▀ ██  ██ ▄▄▄█▀▄▐█ ▌▐▌▐█▐▀▀ ▄           |\n" +
-                            "|     ▐█▌██▐█▐█▄▄▐█▌▐▐███▐█▌ ▐██ ██▌▐█▐█▄▄▌           |\n" +
-                            "|      ▀▀▀▀ ▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀█▄▀▀▀  █ ▀▀▀▀▀▀            |\n" +
-                            "|                                                     |\n" +
-                            "|  Welcome to Stranger Game, the Java Console Game!   |\n" +
-                            "|  Please select an option from the choices below:    |\n" +
-                            "|                                                     |\n" +
-                            "|  Play the Game            [Command: Play]           |\n" +
-                            "|  Exit this Program        [Command: Exit]           |\n" +
-                            "|_____________________________________________________|\n");
+        System.out.println("\n" +
+                "       _______________________________________________________\n" +
+                "       |     ▄▄▌ ▐ ▄▄▄▄ ▄▄▌  ▄▄        ▌ ▄   ▄▄▄             |\n" +
+                "       |     ██  █▌▐▀▄ ▀██  ▐█ ▌      ██ ▐███▀▄ ▀            |\n" +
+                "       |     ██ ▐█▐▐▐▀▀ ██  ██ ▄▄▄█▀▄▐█ ▌▐▌▐█▐▀▀ ▄           |\n" +
+                "       |     ▐█▌██▐█▐█▄▄▐█▌▐▐███▐█▌ ▐██ ██▌▐█▐█▄▄▌           |\n" +
+                "       |      ▀▀▀▀ ▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀█▄▀▀▀  █ ▀▀▀▀▀▀            |\n" +
+                "       |                                                     |\n" +
+                "       |  Welcome to Stranger Game, the Java Console Game!   |\n" +
+                "       |  Please select an option from the choices below:    |\n" +
+                "       |                                                     |\n" +
+                "       |  Play the Game            [Command: Play]           |\n" +
+                "       |  Exit this Program        [Command: Exit]           |\n" +
+                "       |_____________________________________________________|\n");
     }
 
     static void welcome() {
@@ -42,15 +43,15 @@ public class GameMenuTextArt extends GameMenu {
                 "                ██║  ███╗███████║██╔████╔██║█████╗                  \n" +
                 "                ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝                  \n" +
                 "                ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗                \n" +
-                "                 ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝                \n" +
-                "                                                                    \n");
+                "                 ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝                \n");
 
-        System.out.println(" Built by Team NullPointer (Team 5)\n" +
-                " Neill Perry (https://github.com/neillperry)\n" +
-                " Bruce West (https://github.com/BruceBAWest)\n" +
-                " Tapan Trivedi (https://github.com/tapantriv)\n" +
-                " TLG Learning: Capstone Java Project\n" +
-                " https://github.com/NullPointer-Team");
+        System.out.println("\n" +
+                "           Built by Team NullPointer (Team 5)\n" +
+                "           Neill Perry (https://github.com/neillperry)\n" +
+                "           Bruce West (https://github.com/BruceBAWest)\n" +
+                "           Tapan Trivedi (https://github.com/tapantriv)\n" +
+                "           TLG Learning: Capstone Java Project\n" +
+                "           https://github.com/NullPointer-Team");
     }
 
     static void goodbye() {
