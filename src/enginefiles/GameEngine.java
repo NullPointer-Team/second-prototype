68c78138bd5818109a0cc56f1c5648a2aab8a65a
BruceBAWest <brucebawest@gmail.com>
2020-07-16 16:02:30 -0700

    small changes

--- src/enginefiles/GameEngine.java
+++ src/enginefiles/GameEngine.java
@@ -25,8 +25,7 @@ public class GameEngine {
     private String currentRoom;
     private ArrayList<String> inventory;
     private Boolean gameOver = false;
-    private Scanner input;
-    private GameMap gameMap = new GameMap();
+    private final GameMap gameMap = new GameMap();
     private HashMap<String, HashMap<String, String>> rooms;
 
     public void playGame() {
@@ -34,7 +33,7 @@ public class GameEngine {
         rooms = gameMap.rooms;
         currentRoom = "Dining Room";
         inventory = new ArrayList<String>();
-        input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         //TODO: fix rooms.get and showStatus method
         while (!gameOver) {
