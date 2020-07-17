343c4588bcb6ecb2c9ed2f0629935edba2e406ef
neillperry <chainsaw.bucktooth@gmail.com>
2020-07-17 14:30:07 -0700

    created unit test file for GameMenuTextArt

---
+++ test/coregamefiles/GameMenuTextArtTest.java
@@ -0,0 +1,24 @@
+package coregamefiles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameMenuTextArtTest {
+
+    @Test
+    void welcome() {
+    }
+
+    @Test
+    void gameMenuDisplayText() {
+    }
+
+    @Test
+    void gameMenuPrompt() {
+    }
+
+    @Test
+    void goodbye() {
+    }
+}
