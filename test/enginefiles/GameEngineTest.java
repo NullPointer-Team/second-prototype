7c25e6245fe636ba6a049c99f8c78d6f0b2d50db
neillperry <chainsaw.bucktooth@gmail.com>
2020-07-17 09:11:48 -0700

    broke up playGame method in the Game Engine to smaller, testable functions. created blank user tests that I will in for hte Game Engine methods

---
+++ test/enginefiles/GameEngineTest.java
@@ -0,0 +1,24 @@
+package enginefiles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameEngineTest {
+
+    @Test
+    void getCurrentRoom() {
+    }
+
+    @Test
+    void setCurrentRoom() {
+    }
+
+    @Test
+    void getInventory() {
+    }
+
+    @Test
+    void setInventory() {
+    }
+}
