49d00749e5f9619e8561ece1956a8d34923e14af
BruceBAWest <brucebawest@gmail.com>
2020-07-21 15:24:56 -0700

    add red alert to alert art

--- src/coregamefiles/AlertArt.java
+++ src/coregamefiles/AlertArt.java
@@ -1,14 +1,18 @@
 package coregamefiles;
 
+import static coregamefiles.GameTextColors.getAnsiRed;
+import static coregamefiles.GameTextColors.getAnsiReset;
+
 public class AlertArt {
 
     public static void alert() {
-        System.out.println("You won!! Good job!!");
-        System.out.println("\n" +
+        System.out.println(getAnsiRed() +
+                "\n" +
                 " █████  ██      ███████ ██████  ████████ \n" +
                 "██   ██ ██      ██      ██   ██    ██    \n" +
                 "███████ ██      █████   ██████     ██    \n" +
                 "██   ██ ██      ██      ██   ██    ██    \n" +
-                "██   ██ ███████ ███████ ██   ██    ██    \n");
+                "██   ██ ███████ ███████ ██   ██    ██    \n" +
+                getAnsiReset());
     }
 }
