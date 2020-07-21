bd39134adde109132b707e413b570200fd510ad1
tapantriv <tapan_trivedi_4u@yahoo.co.in>
2020-07-21 13:26:20 -0700

    test

---
+++ src/client/GameEngineClient.java
@@ -0,0 +1,19 @@
+/*
+ *      Stranger Game
+ *      Team NullPointer (Team 5)
+ *      https://github.com/NullPointer-Team
+ *      Neill Perry (https://github.com/neillperry)
+ *      Bruce West (https://github.com/BruceBAWest)
+ *      Tapan Trivedi (https://github.com/tapantriv)
+ *      TLG Learning: Capstone Project
+ *      https://github.com/NullPointer-Team/prototype
+ */
+
+package client;
+
+public class GameEngineClient {
+    public static void main(String[] args) throws Exception {
+        coregamefiles.GameMenu gameMenu = new coregamefiles.GameMenu();
+        gameMenu.startGame();
+    }
+}
