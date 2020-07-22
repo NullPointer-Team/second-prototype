ae80e8b285e77e4c3beb6254bd97aa762f003813
tapantriv <tapan_trivedi_4u@yahoo.co.in>
2020-07-22 15:26:32 -0700

    fix bug where user music play continuously,not it will play one time but it is temp solution

---
+++ src/coregamefiles/GameHintMap.java
@@ -0,0 +1,20 @@
+//package coregamefiles;
+//import static coregamefiles.GameTextColors.getAnsiRed;
+//import static coregamefiles.GameTextColors.getAnsiReset;
+//
+//public class GameHintMap {
+//    public static void alert() {
+//        System.out.println(getAnsiRed() +
+//                "--------------------------------------------------------------------------" +
+//                "|" +"ATRIUM"        +"|" +   "BREAKFAST NOOK" +"|" +
+//                "|"                  +"|" +
+//                "|" +"fighting skill"+"|" +
+//                "\n"+
+//
+//
+//
+//
+//
+//        getAnsiReset());
+//}
+//}
