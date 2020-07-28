3f96b0feed19b5ab13af34a96ecbe6bb1b9f582f
tapantriv <tapan_trivedi_4u@yahoo.co.in>
2020-07-28 16:27:03 -0700

    fixed bug but not able to add total

--- src/point/BonusPoint.java
+++ src/point/BonusPoint.java
@@ -2,15 +2,29 @@ package point;
 
 import enginefiles.GameEngine;
 import music.Music;
-//try to use OOP Concept so I do not have to repeat ANSI soI extends
+
+import java.util.ArrayList;
+import java.util.List;
+
+//try to use OOP Concept so I do not have to repeat ANSI so I extends
 public class BonusPoint extends Music {
-    GameEngine game = new GameEngine();
+//    GameEngine game = new GameEngine();
     int point =0;
+    List<Integer> totalPoint= new ArrayList<Integer>();
+
+//    int points = totalPoint+point;
+
     public void gamePoint(){
 
             point = point+10;
+            totalPoint.add(point);
+//
+//        System.out.println("Total Point:"+totalPoint);
             System.out.println(ANSI_GREEN +"Congratulations!!!!! You got Bonus "+ point +" Point."+ANSI_RESET);
-        }
+     for(int point :totalPoint){
+         System.out.println("Total Point:"+point);
+     }
+    }
 
     }
 
